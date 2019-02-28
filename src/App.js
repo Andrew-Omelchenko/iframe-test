86d3f9bc5bc8746f66c28765083dca0cd9011166
Andrew Omelchenko <andrewom43@gmail.com>
2019-02-28 23:13:17 +0200

    Add Instagram support

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FacebookProvider, EmbeddedPost } from 'react-facebook';
+import InstagramEmbed from 'react-instagram-embed';
+
+//Styles
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div
+            className="wrapper"
+        >
+            <InstagramEmbed
+                className="instagramStyled"
+                url="https://www.instagram.com/p/BuWrKTKBxhL/?utm_source=ig_web_button_share_sheet"
+                containerTagName='span'
+                hideCaption={false}
+            />
+        </div>
     );
   }
 }
