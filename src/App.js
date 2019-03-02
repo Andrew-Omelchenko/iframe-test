95641b65074c38b8e8ca8f8ea256b7e384549166
Andrew Omelchenko <andrewom43@gmail.com>
2019-03-02 13:22:48 +0200

    Fix indents

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import { FacebookProvider, EmbeddedPost } from 'react-facebook';
 import './App.css';
 
 class App extends Component {
-    isMobile() {
-        const isInAppMode = ("standalone" in navigator && navigator.standalone) || (window.chrome && window.top.chrome.app && window.top.chrome.app.isInstalled);
+  isMobile() {
+    const isInAppMode = ("standalone" in navigator && navigator.standalone) || (window.chrome && window.top.chrome.app && window.top.chrome.app.isInstalled);
 
-        const ua = navigator.userAgent;
-        // memoized values
-        const isIphone = ua.indexOf('iPhone') !== -1 || ua.indexOf('iPod') !== -1;
-        const isIpad = ua.indexOf('iPad') !== -1;
-        const isAndroid = ua.indexOf('Android') !== -1;
-        console.log('iPhone:', isIphone,'iPad:', isIpad, 'Android:', isAndroid);
-        return isIphone || isIpad || isAndroid;
-    }
+    const ua = navigator.userAgent;
+    // memoized values
+    const isIphone = ua.indexOf('iPhone') !== -1 || ua.indexOf('iPod') !== -1;
+    const isIpad = ua.indexOf('iPad') !== -1;
+    const isAndroid = ua.indexOf('Android') !== -1;
+    console.log('iPhone:', isIphone, 'iPad:', isIpad, 'Android:', isAndroid);
+    return isIphone || isIpad || isAndroid;
+  }
 
   render() {
     console.log('window', typeof window);
@@ -23,23 +23,23 @@ class App extends Component {
     console.log('device pixel ratio', window.devicePixelRatio);
       let width = 500;
     if (this.isMobile() || window.innerWidth < 548) {
-        width = 350;
+      width = 350;
     }
     console.log('calc width', width);
 
     return (
-        <div>
-            <FacebookProvider
-                class="provider"
-                appId="390050348463189"
-            >
-                <EmbeddedPost
-                    class="post"
-                    href="https://www.facebook.com/OlenaDobrzhanska/posts/2043448672390166?__tn__=-UC-R"
-                    width={window.innerWidth - 24}
-                />
+      <div>
+        <FacebookProvider
+          class="provider"
+          appId="390050348463189"
+        >
+          <EmbeddedPost
+            class="post"
+            href="https://www.facebook.com/OlenaDobrzhanska/posts/2043448672390166?__tn__=-UC-R"
+            width={window.innerWidth - 24}
+          />
         </FacebookProvider>
-        </div>
+      </div>
     );
   }
 }
